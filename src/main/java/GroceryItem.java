2128af32079176b63e958209fdf042c578eb0c84
MNinh <msninh2@gmail.com>
2021-04-12 10:46:34 -0400

    checkpoint

---
+++ src/main/java/GroceryItem.java
@@ -0,0 +1,46 @@
+public class GroceryItem {
+
+    private String name;
+    private Double price;
+    private String type;
+    private String expiration;
+
+    GroceryItem(String name, Double price, String type, String expiration){
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.expiration = expiration;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(String expiration) {
+        this.expiration = expiration;
+    }
+}
